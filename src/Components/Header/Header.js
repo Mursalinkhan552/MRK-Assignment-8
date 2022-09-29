6607515b0734b1fbc3d7f08affb20ddf081a7b0f
unknown <mursalinkhan552@gmail.com>
2022-09-29 08:47:20 -0700

    Second Commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/logo.png'
+
+const Header = () => {
+    return (
+        <nav className='nav'>
+            <div className="nav-logo">
+                
+                <h1>Ultrafit</h1>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
